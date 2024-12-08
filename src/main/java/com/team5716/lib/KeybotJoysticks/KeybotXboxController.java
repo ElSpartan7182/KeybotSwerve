4cb71b8a41092d0e9dc428434d2285ef2795ff46
ElSpartan7182 <alumnoruben3b@gmail.com>
2024-12-08 13:33:01 -0600

    Actualización en XboxController

--- src/main/java/com/team5716/lib/KeybotJoysticks/KeybotXboxController.java
+++ src/main/java/com/team5716/lib/KeybotJoysticks/KeybotXboxController.java
@@ -6,9 +6,6 @@ import edu.wpi.first.math.MathUtil;
 import edu.wpi.first.wpilibj.XboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 
-/**
- * Custom class for Xbox Controller use
- */
 public class KeybotXboxController extends XboxController {
 
 	public Trigger btn_A = new Trigger(super::getAButton);
@@ -25,8 +22,6 @@ public class KeybotXboxController extends XboxController {
 	public Trigger btn_LeftStick = new Trigger(super::getLeftStickButton);
 	public Trigger btn_RightStick = new Trigger(super::getRightStickButton);
 
-	// The POV angles start at 0 in the up direction, and increase clockwise (eg
-	// right is 90, upper-left is 315).
 	private final int D_PAD_NORTH = 0;
 	private final int D_PAD_NORTH_EAST = 45;
 	private final int D_PAD_EAST = 90;
@@ -60,69 +55,20 @@ public class KeybotXboxController extends XboxController {
 	public DoubleSupplier axis_LeftTrigger = () -> super.getLeftTriggerAxis();
 	public DoubleSupplier axis_RightTrigger = () -> super.getRightTriggerAxis();
 
-	/**
-	 * Set the value that the triggers must be at to register as true while being
-	 * used as a button. A value of zero will make the trigger act as a hair trigger
-	 * (will activate at the slightest touch) and a value of one will make the
-	 * trigger be held all the way down to activate.
-	 * <p>
-	 * Default value is 0.5
-	 *
-	 * @param threshold
-	 *            for trigger button
-	 */
 	public void setTriggerPressThreshold(double threshold) {
 		TRIGGER_PRESS_THRESHOLD = MathUtil.clamp(threshold, 0, 0.99);
 	}
-
-	/**
-	 * Set the deadband for the left joystick.
-	 * <p>
-	 * Default value is 0.1
-	 *
-	 * @param deadband
-	 *            to set for the left joystick
-	 */
 	public void setLeftDeadband(double deadband) {
 		LEFT_DEADBAND = deadband;
 	}
-
-	/**
-	 * Set the deadband for the right joystick.
-	 * <p>
-	 * Default value is 0.1
-	 *
-	 * @param deadband
-	 *            to set for the right joystick
-	 */
 	public void setRightDeadband(double deadband) {
 		RIGHT_DEADBAND = deadband;
 	}
-
-	/**
-	 * Construct an instance of an SN_XboxController.
-	 *
-	 * @param port
-	 *            The port index on the Driver Station that the controller is
-	 *            plugged into.
-	 * @param leftDeadband
-	 *            The deadband for the left joystick.
-	 * @param rightDeadband
-	 *            The deadband for the right joystick.
-	 */
 	public KeybotXboxController(int port, double leftDeadband, double rightDeadband) {
 		super(port);
 		LEFT_DEADBAND = leftDeadband;
 		RIGHT_DEADBAND = rightDeadband;
-	}
-
-	/**
-	 * Construct an instance of an SN_XboxController.
-	 *
-	 * @param port
-	 *            The port index on the Driver Station that the controller is
-	 *            plugged into.
-	 */
+    }
 	public KeybotXboxController(int port) {
 		super(port);
 	}
